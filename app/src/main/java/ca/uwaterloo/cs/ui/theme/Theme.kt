844d450b1db8a3a2bdb539d3919d78aa35498a9a
Saket <saket@squareup.com>
2022-05-25 02:16:31 -0400

    Add Instagram gradient colors

--- app/src/main/java/ca/uwaterloo/cs/ui/theme/Theme.kt
+++ app/src/main/java/ca/uwaterloo/cs/ui/theme/Theme.kt
@@ -7,11 +7,22 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun HelloAndroidTheme(content: @Composable () -> Unit) {
+  val darkColors = darkColors(
+    background = Color.VeryDarkGray,
+    surface = Color.VeryDarkGray,
+    primary = Color.VeryDarkGray,
+  )
+  val lightColors = lightColors(
+    primary = Color.White,
+    onPrimary = Color.DarkGray,
+    surface = Color.DarkGray,
+  )
   MaterialTheme(
-    colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
+    colors = if (isSystemInDarkTheme()) darkColors else lightColors,
     content = {
       val defaultTextStyle = LocalTextStyle.current.copy(
         color = MaterialTheme.colors.onBackground
@@ -22,3 +33,8 @@ fun HelloAndroidTheme(content: @Composable () -> Unit) {
     }
   )
 }
+
+val Color.Companion.VeryDarkGray get() = Color(0xFF121212)
+val Color.Companion.InstagramOrange get() = Color(0xFFFF7A00)
+val Color.Companion.InstagramPeach get() = Color(0xFFFF0069)
+val Color.Companion.InstagramPurple get() = Color(0xFFD300C5)
